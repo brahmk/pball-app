b19dd9e1896e418fb06cfb9e2a4341cad673debf
brahmk <brahm.kelly@gmail.com>
2022-06-15 16:51:10 -0400

    1st birds ok

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,5 +1,23 @@
 import React from "react";
 
 export default function Hero() {
-  return <div className="hero"></div>;
+  return (
+    <div className="hero">
+      <div class="bird-container bird-container--one">
+        <div class="bird bird--one"></div>
+      </div>
+
+      <div class="bird-container bird-container--two">
+        <div class="bird bird--two"></div>
+      </div>
+
+      <div class="bird-container bird-container--three">
+        <div class="bird bird--three"></div>
+      </div>
+
+      <div class="bird-container bird-container--four">
+        <div class="bird bird--four"></div>
+      </div>
+    </div>
+  );
 }
